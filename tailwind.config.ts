76e98f339e93842180564d6105b921f5f761d854
senseiGai <swerss564@gmail.com>
2024-05-01 19:23:56 +0500

    CHANGES IN CONFIG AND MAIN SCREEN

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,14 +3,15 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-			
+				'Proxima Soft': ['Proxima Soft','sans-serif']
 			},
 			colors: {
 				primary: {
 					dark: '#444444',
 					brown: '#BCA086',
 					light: '#FFFFFF',
-					grey: '#B8B8B8'
+					grey: '#B8B8B8',
+					bggrey: '#F4F4F4'
 				}
 			}
 		}
