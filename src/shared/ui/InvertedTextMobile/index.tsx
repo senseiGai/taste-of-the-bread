66c349c1543e7c467be8d4e1c35e84eac69daeea
senseiGai <swerss564@gmail.com>
2024-05-02 13:17:14 +0500

    TASTE WAS IMPORVED

---
+++ src/shared/ui/InvertedTextMobile/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+interface IInvertedText {
+    invertedTextType: 'mobile'; 
+    letter: string;
+    margin?: string; 
+}
+
+export const InvertedText: React.FC<IInvertedText> = ({ invertedTextType, letter, margin = '' }, ref) => {
+    const invertedTextClass = `${styles.letter} ${styles[invertedTextType]} ${margin ? margin : ''}`;
+
+    return (
+        <span className={invertedTextClass} ref={ref}>
+            {letter}
+        </span>
+    );
+};
--- src/widgets/Screens/MainMobile/index.tsx
+++ src/widgets/Screens/MainMobile/index.tsx
@@ -1,29 +1,32 @@
-import {useAnimateSmallEllipse} from '@shared/ui/lib/hooks/Animations/useAnimateSmallEllipse'
-import { useAnimatePathLetters } from '@shared/ui/lib/hooks/Animations/useAnimatePathLetters'
+import React, { useRef } from 'react';
+import { InvertedText } from '@shared/ui/InvertedTextMobile';
+import { useAnimateSmallEllipse } from '@shared/ui/lib/hooks/Animations/useAnimateSmallEllipse';
+import { letters } from '@shared/ui/lib/content/invertedLettersContent';
 
-import big_ellipse from '@assets/Main/big_ellipse.svg'
-import small_ellipse from '@assets/Main/small_ellipse.svg'
-import wheat from '@assets/Main/wheat.svg'
-import bread from '@assets/Main/bread.svg'
+import big_ellipse from '@assets/Main/big_ellipse.svg';
+import small_ellipse from '@assets/Main/small_ellipse.svg';
+import wheat from '@assets/Main/wheat.svg';
+import bread from '@assets/Main/bread.svg';
 
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 
 export default function MainMobile() {
-  const smallEllipseRef = useAnimateSmallEllipse(); 
+  const smallEllipseRef = useAnimateSmallEllipse();
 
 
   return (
     <div className={styles.container}>
       <div className={`${styles.container__images} flex flex-row`}>
         <img src={small_ellipse.src} ref={smallEllipseRef} alt="small-ellipse" className={styles.container__images_small_ellipse} />
-        <span className={styles.container__images_taste}>c</span>
-        <span className={styles.container__images_taste}>y</span>
-        <span className={styles.container__images_taste}>k</span>
-        <span className={styles.container__images_taste}>B</span>
+        <div className="flex items-center flex-col-reverse mt-5 ml-7">
+          {letters.map((letter, index) => (
+            <InvertedText key={index} letter={letter} invertedTextType="mobile" />
+          ))}
+        </div>
         <img src={bread.src} alt="bread" className={styles.container__images_bread}/>
         <img src={big_ellipse.src} alt="big-ellipse" className={styles.container__images_big_ellipse}/>
         <img src={wheat.src} alt="wheat" className={styles.container__images_wheat} />
       </div>
     </div>
-  )
+  );
 }
